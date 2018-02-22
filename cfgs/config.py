08811b5d66d5d58384be8e37b07da5e06bc0aa04
JesseYang <jesse.yang1985@gmail.com>
2018-02-22 10:34:35 +0800

    u

--- cfgs/config.py
+++ cfgs/config.py
@@ -1,4 +1,4 @@
 from .config_voc_0712 import cfg
 
 # step based learning rate schedule
-cfg.lr_schedule = [(0, 1e-3), (4e4, 1e-4), (48e3, 1e-5)]
+cfg.lr_schedule = [(0, 1e-3), (6e4, 1e-4), (7e4, 1e-5)]
